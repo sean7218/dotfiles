60d1eb8e9be6b63e6f0bb05270774486b11cab3f
Sean Zhang <sean7218@gmail.com>
2023-10-01 22:12:03 -0400

    added Multicursor

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -2,6 +2,24 @@ local overrides = require("custom.configs.overrides")
 
 ---@type NvPluginSpec[]
 local plugins = {
+  -- lazy.nvim:
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      'smoka7/hydra.nvim',
+    },
+    opts = {},
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+      {
+        mode = { 'v', 'n' },
+        '<Leader>m',
+        '<cmd>MCstart<cr>',
+        desc = 'Create a selection for selected text or word under the cursor',
+      },
+    },
+  },
 
   -- Override plugin definition options
   {
