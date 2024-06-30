66746b6cfe9c835a8f9499f547144c2828be1767
Sean Zhang <sean7218@gmail.com>
2024-06-30 17:13:54 -0400

    updated home row mod

--- .config/qmk/keyboards/drop/sense75/keymaps/sean7218/keymap.c
+++ .config/qmk/keyboards/drop/sense75/keymaps/sean7218/keymap.c
@@ -14,6 +14,11 @@
 #define LSC_U LSG(KC_UP)
 #define LSC_D LSG(KC_DOWN)
 
+#define LGUIA LGUI_T(KC_A)
+#define LALTS LALT_T(KC_S)
+#define LSFTD LSFT_T(KC_D)
+#define LCTLF LALT_T(KC_F)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
      * ┌───┐┌───┬───┬───┬───┐┌───┬───┬───┬───┐┌───┬───┬───┬───┐┌───┐┌───┐
@@ -36,15 +41,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        KC_ESC,  LGUIA,   LALTS,   LSFTD,   LCTLF,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,
         KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_MPLY,
-        _______, LSC_1,   LSC_2,   LSC_3,   LSC_4,   LSC_5,   _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          _______,
-        _______, RGB_TOG, LSC_U,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_HOME,
-        _______, LSC_L,   LSC_D,   LSC_R,   _______, _______, _______, _______, _______, _______, _______, _______,          _______,          KC_END,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          _______,
+        _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_HOME,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          KC_END,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______, _______, _______,                            QK_LEAD,                             KC_APP,  _______, _______, _______, _______, _______
     )
