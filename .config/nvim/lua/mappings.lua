66092a30a42404c8ed56f3154701d3a880db8a44
Sean Zhang <sean7218@gmail.com>
2024-09-08 15:23:38 -0400

    add move lines

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -29,3 +29,6 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("v", "<A-J>", ":m '>+1<CR>gv=gv", { desc = "move line down" })
+vim.keymap.set("v", "<A-K>", ":m '<-2<CR>gv=gv", { desc = "move line up" })
