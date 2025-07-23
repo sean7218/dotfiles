1e2cf242cf16239e7c9a9248342577267a8501b6
Sean Zhang <sean7218@gmail.com>
2025-07-23 18:44:39 -0400

    updated nvim

--- nvim/lua/config/lsp-diagnostic.lua
+++ nvim/lua/config/lsp-diagnostic.lua
@@ -13,10 +13,10 @@ vim.diagnostic.config {
     underline = { severity = vim.diagnostic.severity.ERROR },
     signs = vim.g.have_nerd_font and {
         text = {
-            [vim.diagnostic.severity.ERROR] = '',
-            [vim.diagnostic.severity.WARN] = '',
-            [vim.diagnostic.severity.INFO] = '',
-            [vim.diagnostic.severity.HINT] = '',
+            [vim.diagnostic.severity.ERROR] = '󰅚 ',
+            [vim.diagnostic.severity.WARN] = '󰀪 ',
+            [vim.diagnostic.severity.INFO] = '󰋽 ',
+            [vim.diagnostic.severity.HINT] = '󰌶 ',
         },
     } or {},
     virtual_text = false,
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,28 +1,46 @@
 return {
     'neovim/nvim-lspconfig',
     dependencies = {
-        "j-hui/fidget.nvim"
+        -- Automatically install LSPs and related tools to stdpath for Neovim
+        -- Mason must be loaded before its dependents so we need to set it up here.
+        -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+        { 'mason-org/mason.nvim', opts = {} },
+        'mason-org/mason-lspconfig.nvim',
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
+
+
+        -- Useful status updates for LSP.
+        { 'j-hui/fidget.nvim',    opts = {} },
+
+        -- Allows extra capabilities provided by blink.cmp
+        'saghen/blink.cmp',
     },
     config = function()
         local lspconfig = require("lspconfig")
+        local capabilities = require('blink.cmp').get_lsp_capabilities()
         lspconfig.sourcekit.setup({
             cmd = {
-                "env",
-                "SOURCEKIT_LOGGING=3",
                 "sourcekit-lsp"
             },
             cmd_env = {
                 SOURCEKIT_LOGGING = '3'
             },
+            capabilities = capabilities
         })
         lspconfig.rust_analyzer.setup({
             settings = {
                 ['rust-analyzer'] = {
                     diagnostics = {
-                        enable = false;
+                        enable = false,
                     }
                 }
             }
+        })
+        lspconfig["pyright"].setup({
+            capabilities = capabilities
+        })
+        lspconfig["ts_ls"].setup({
+            capabilities = capabilities
         })
     end
 }
